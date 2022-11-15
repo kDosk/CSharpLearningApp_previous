a8a379c06e1ef0333edce2ef9235fae7f1767efe
Niyaz <nlatypov6@gmail.com>
2022-11-15 10:03:07 +0500

    marg

--- CSharpLearningApp/Niyaz/MainWindowArifm.xaml.cs
+++ CSharpLearningApp/Niyaz/MainWindowArifm.xaml.cs
@@ -23,5 +23,10 @@ namespace CSharpLearningApp.Niyaz
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
