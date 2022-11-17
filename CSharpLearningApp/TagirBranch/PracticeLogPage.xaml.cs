3c48c15e4d8bad7e20986ed7663d62238df10628
Tagir <tagirsyndukov@gmail.com>
2022-11-17 18:03:27 +0500

    Последний подраздел

--- CSharpLearningApp/TagirBranch/PracticeLogPage.xaml.cs
+++ CSharpLearningApp/TagirBranch/PracticeLogPage.xaml.cs
@@ -23,6 +23,70 @@ namespace CSharpLearningApp.TagirBranch
         public PracticeLogPage()
         {
             InitializeComponent();
+            TextBlock1.Text = "Операция логического сложения или логическое ИЛИ. Возвращает true, если хотя бы один из операндов возвращает true.";
+            TextBlock2.Text = "Операция логического сложения. Возвращает true, если хотя бы один из операндов возвращает true.";
+            TextBlock3.Text = "Операция логического отрицания. Производится над одним операндом и возвращает true, если операнд равен false. Если операнд равен true, то операция возвращает false:\r\n\r\n";
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MainPage());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Textbox1.Text == TextBlock1.Text)
+            {
+                MessageBox.Show("Правильно1");
+            }
+            if (Textbox2.Text == TextBlock2.Text)
+            {
+                MessageBox.Show("Правильно3");
+            }
+            if (Textbox3.Text == TextBlock3.Text)
+            {
+                MessageBox.Show("Правильно2");
+            }
+            if (Textbox1.Text == TextBlock1.Text && Textbox2.Text == TextBlock2.Text && Textbox3.Text == TextBlock3.Text)
+            {
+                MessageBox.Show("Всё правильно!");
+                NavigationService.Navigate(new MainPage());
+            }
+            else
+            {
+                MessageBox.Show("Где-то ошибка, переходим к теории");
+                NavigationService.Navigate(new TheorySravpage());
+            }
+        }
+
+        private void TextBlock1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragDrop.DoDragDrop(TextBlock1, TextBlock1.Text, DragDropEffects.Copy);
+        }
+
+        private void TextBlock2_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragDrop.DoDragDrop(TextBlock2, TextBlock2.Text, DragDropEffects.Copy);
+        }
+
+        private void TextBlock3_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragDrop.DoDragDrop(TextBlock3, TextBlock3.Text, DragDropEffects.Copy);
+        }
+
+        private void Textbox1_Drop(object sender, DragEventArgs e)
+        {
+            Textbox1.Text = (string)e.Data.GetData(DataFormats.Text);
+        }
+
+        private void Textbox2_Drop(object sender, DragEventArgs e)
+        {
+            Textbox2.Text = (string)e.Data.GetData(DataFormats.Text);
+        }
+
+        private void Textbox3_Drop(object sender, DragEventArgs e)
+        {
+            Textbox3.Text = (string)e.Data.GetData(DataFormats.Text);
         }
     }
 }
--- CSharpLearningApp/TagirBranch/TheoryPage.xaml.cs
+++ CSharpLearningApp/TagirBranch/TheoryPage.xaml.cs
@@ -38,7 +38,7 @@ namespace CSharpLearningApp.TagirBranch
 
         private void Click_ValidPage(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new ValidationPage());
+            NavigationService.Navigate(new ValidationLogPage());
         }
 
         private void ButtonExit_Click(object sender, RoutedEventArgs e)
