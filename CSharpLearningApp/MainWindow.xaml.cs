9365557b74465158955eac256e5a0ed48463699b
Камиля Сафина <ksafina435@gmail.com>
2022-11-21 01:31:57 +0500

    pfpfpfp

--- CSharpLearningApp/MainWindow.xaml.cs
+++ CSharpLearningApp/MainWindow.xaml.cs
@@ -31,7 +31,7 @@ namespace CSharpLearningApp
             switch ((sender as Button).Content)
             {
                 case "Переменные и константы":
-                    ShowWindow(new Pages.MainKamilysWindow());
+                    ShowWindow(new MainKamilysWindow());
                     break;
                 case "Типы данных":
                     ShowWindow(new Window());
--- CSharpLearningApp/Pages/MainKamilysWindow.xaml.cs
+++ CSharpLearningApp/Pages/MainKamilysWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharpLearningApp.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
