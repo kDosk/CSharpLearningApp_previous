0f36d11c05c19ebe966d4ec0f4bd3cb974d4fed4
Камиля Сафина <ksafina435@gmail.com>
2022-11-20 23:54:57 +0500

    d

--- CSharpLearningApp/Pages/KamilyaOcenka2.xaml.cs
+++ CSharpLearningApp/Pages/KamilyaOcenka2.xaml.cs
@@ -23,34 +23,34 @@ namespace CSharpLearningApp.Pages
         public KamilyaOcenka2()
         {
             InitializeComponent();
-            { 
-            if (KamilyaOcenki.ocenka2 <= 2)
             {
-                OcenkaText.Text = ("Ваша оценка 2\r" + "Тест провален!");
-            }
+                if (KamilyaOcenki.ocenka2 <= 2)
+                {
+                    OcenkaText.Text = ("Ваша оценка 2\r" + "Тест провален!");
+                }
 
-            if (KamilyaOcenki.ocenka2 == 3)
-            {
-                OcenkaText.Text = ("Ваша оценка 3\r" + "Тест пройден удовлетворительно!");
-            }
+                if (KamilyaOcenki.ocenka2 == 3)
+                {
+                    OcenkaText.Text = ("Ваша оценка 3\r" + "Тест пройден удовлетворительно!");
+                }
 
-            if (KamilyaOcenki.ocenka2 == 4)
-            {
-                OcenkaText.Text = ("Ваша оценка 4\r" + "Тест пройден хорошо!");
-            }
+                if (KamilyaOcenki.ocenka2 == 4)
+                {
+                    OcenkaText.Text = ("Ваша оценка 4\r" + "Тест пройден хорошо!");
+                }
 
-            if (KamilyaOcenki.ocenka2 == 5)
-            {
-                OcenkaText.Text = ("Ваша оценка 5\r" + "Тест пройден отлично!");
+                if (KamilyaOcenki.ocenka2 == 5)
+                {
+                    OcenkaText.Text = ("Ваша оценка 5\r" + "Тест пройден отлично!");
+                }
             }
         }
-    }
 
-    private void Button_Click(object sender, RoutedEventArgs e)
-    {
-        Manager.MainFrame.GoBack();
-        Manager.MainFrame.GoBack();
-        Manager.MainFrame.GoBack();
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.GoBack();
+            Manager.MainFrame.GoBack();
+            Manager.MainFrame.GoBack();
+        }
     }
- }
 }
