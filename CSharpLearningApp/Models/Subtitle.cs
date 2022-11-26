e00b8c9a3add286e3349f848280edddc7e79989d
kDosk <stuser.ls@gmail.com>
2022-11-26 23:27:47 +0500

    Corrections made

---
+++ CSharpLearningApp/Models/Subtitle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearningApp.Models
+{
+	internal class Subtitle
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+		public int PageTitleId { get; set; }
+		public PageTitle PageTitle { get; set; }
+	}
+}
