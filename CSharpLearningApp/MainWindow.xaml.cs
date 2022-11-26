2719ed93cee97a2e683b34a4b97451506550ec59
kDosk <stuser.ls@gmail.com>
2022-11-26 16:21:06 +0500

    Application updated to .NET 6

--- CSharpLearningApp/MainWindow.xaml.cs
+++ CSharpLearningApp/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CSharpLearningApp.Classes.MessageService;
+using CSharpLearningApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +17,115 @@ using System.Windows.Shapes;
 
 namespace CSharpLearningApp
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
-	}
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        private void NavigateButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Переход к окну по названию нажатой кнопки
+            if (UserModel.CurrentUser != null)
+            {
+                switch ((sender as Button).Content)
+                {
+                    case "Переменные и константы":
+                        ShowWindow(new Window());
+                        break;
+                    case "Типы данных":
+                        ShowWindow(new Window());
+                        break;
+                    case "Арифметические вычисления":
+                        ShowWindow(new Window());
+                        break;
+                    case "Операции присваивания":
+                        ShowWindow(new Window());
+                        break;
+                    case "Преобразование базовых типов данных":
+                        ShowWindow(new Window());
+                        break;
+                    case "Условные выражения":
+                        ShowWindow(new Window());
+                        break;
+                    case "Циклы":
+                        ShowWindow(new Window());
+                        break;
+                    case "Массивы":
+                        ShowWindow(new Window());
+                        break;
+                    case "Методы":
+                        ShowWindow(new Window());
+                        break;
+                    case "Оператор return":
+                        ShowWindow(new Window());
+                        break;
+                    case "Перечисления enum":
+                        ShowWindow(new Window());
+                        break;
+                    default:
+                        MessageBox.Show("Ошибка выполнения.");
+                        break;
+                }
+            }
+            else
+            {
+                MessageService.ShowError("Вход не выполнен.");
+            }
+        }
+
+        /// <summary>
+        /// Метод открытия окна
+        /// </summary>
+        /// <param name="currentWindow">Название окна</param>
+        private void ShowWindow(Window currentWindow)
+        {
+            currentWindow.ShowDialog();
+        }
+
+        private void SignUp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SignInButton_Checked(object sender, RoutedEventArgs e)
+        {
+            gridSignIn.Visibility = Visibility.Visible;
+            gridSignUp.Visibility = Visibility.Hidden;
+        }
+
+        private void SignUpButton_Checked(object sender, RoutedEventArgs e)
+        {
+            gridSignUp.Visibility = Visibility.Visible;
+            gridSignIn.Visibility = Visibility.Hidden;
+        }
+
+        private void SignIn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowModal_Click(object sender, RoutedEventArgs e)
+        {
+            ShowModal();
+        }
+
+        private void HideModal_Click(object sender, RoutedEventArgs e)
+        {
+            HideModal();
+        }
+
+        private void ShowModal()
+        {
+            AuthModalWindow.IsOpen = true;
+        }
+        private void HideModal()
+        {
+            AuthModalWindow.IsOpen = false;
+        }
+    }
 }
